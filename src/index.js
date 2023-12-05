b5f45338b7bc313ae9210d58681be986767a5918
Merima Dedic <merimaxdedic@gmail.com>
2023-12-04 20:42:12 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('%c HI', 'color: firebrick')
+function fetchAndDisplayBreeds() {
+    fetch(breedUrl)
+      .then(response => response.json())
+      .then(data => {
+        const breedList = document.getElementById('breedList');
+        const breedFilter = document.getElementById('breedFilter');
+  
+        function filterBreeds(selectedLetter) {
+          if (selectedLetter === 'all') {
+            return Object.keys(data.message);
+          }
+          return Object.keys(data.message).filter(breed => breed.startsWith(selectedLetter));
+        }
+  
+        breedFilter.addEventListener('change', function() {
+          const selectedLetter = this.value;
+          const filteredBreeds = filterBreeds(selectedLetter);
+  
+          breedList.innerHTML = ''; 
+  
+          filteredBreeds.forEach(breed => {
+            const liElement = document.createElement('li');
+            liElement.textContent = breed;
+            breedList.appendChild(liElement);
+          });
+        });
+  
+        
+        const allBreeds = filterBreeds('all');
+        allBreeds.forEach(breed => {
+          const liElement = document.createElement('li');
+          liElement.textContent = breed;
+          breedList.appendChild(liElement);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', fetchAndDisplayBreeds);
